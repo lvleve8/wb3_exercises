35092ea6e292fce792afe727a9c8ac4ebeca3fd7
yvette rodeo <yrodeo@yearup.org>
2024-04-30 13:08:38 -0500

    first commit

---
+++ functions/simple_functions.js
@@ -0,0 +1,23 @@
+"use strict"
+
+function favoriteThings(){
+
+    console.log("my name is yvette");
+     console.log ("my favorite movie is Scott pilgrim VS The World");
+     console.log ("my favorite band is Radiohead");
+
+}
+
+function whyImHere(){
+    console.log("I am here to learn about HTML,CSS Javascript");
+}
+
+
+function favoritePlace(){
+    console.log(" My favorite place is Marshalls i find clothes or many  items for a discounted price");
+}
+
+//calling/invoking our function 
+favoriteThings();
+whyImHere();
+favoritePlace();
